f94ebd5c9fbce1a2eaab9772e70352a4c3591507
joseph <josephlin117@hotmail.com>
2023-12-15 12:24:50 +0800

    update bash

--- NN.sh
+++ NN.sh
@@ -4,9 +4,11 @@ TIME_HOME="/home/joseph/Projects/Optiver---Trading-at-the-close"
 TF_HOME="/home/joseph/anaconda3/envs/kaggle/bin"
 TIME_NOW=`date +"%Y-%m-%d_%H-%M-%S"`
 
+MODEL_NAME=NN184
+
 #${TF_HOME}/python -u ${TIME_HOME}/LGB/LGB.py
 #${TF_HOME}/python -u ${TIME_HOME}/NN/NN161.py
 
-nohup ${TF_HOME}/python -u ${TIME_HOME}/NN/NN161.py  > ${TIME_HOME}/bash_logs/NN161_${TIME_NOW}.log 2>&1 &
+nohup ${TF_HOME}/python -u ${TIME_HOME}/NN/${MODEL_NAME}.py  > ${TIME_HOME}/bash_logs/${MODEL_NAME}_${TIME_NOW}.log 2>&1 &
 
 
--- baseline.sh
+++ baseline.sh
@@ -4,7 +4,7 @@ TIME_HOME="/home/joseph/Projects/Optiver---Trading-at-the-close"
 TF_HOME="/home/joseph/anaconda3/envs/kaggle/bin"
 TIME_NOW=`date +"%Y-%m-%d_%H-%M-%S"`
 
-MODEL_NAME=Feats164
+MODEL_NAME=Feats184
 
 nohup ${TF_HOME}/python -u ${TIME_HOME}/${MODEL_NAME}/${MODEL_NAME}.py  > ${TIME_HOME}/bash_logs/${MODEL_NAME}_${TIME_NOW}.log 2>&1 &
 
